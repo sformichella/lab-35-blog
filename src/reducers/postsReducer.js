27433373d4b4afd7b595ba174464e72e6354b759
sformichella <sformichella@gmail.com>
2021-02-17 14:43:38 -0800

    postsReducer

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -1,1 +1,28 @@
 
+import { CREATE_POST, DELETE_POST } from '../actions/postActions';
+
+export const initialState = {
+  posts: []
+};
+
+export const reducer = (state, action) => {
+  switch(action.switch) {
+    case CREATE_POST:
+      return {
+        ...state,
+        dogs: [...state.posts, action.payload]
+      };
+    case DELETE_POST: {
+      const posts = state
+        .posts
+        .filter(post => post.body !== action.postBody);
+
+      return {
+        ...state,
+        posts
+      };
+    }
+    default:
+      return state;
+  }
+};
